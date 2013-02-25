1180e5a9c09404fa8dd6f3a3a692fd7961425798
Gennadii Saltyshchak <GennadyGS@gmail.com>
2013-02-25 10:29:23 +0200

    Added SelDephh UciEnfineInfoField

---
+++ Source/UciEngineLib/UciInfoEventFields/SelDepthField.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessBombDetector.UciInfoEventFields
+{
+  public class SelDepthField :UciInfoEventField
+  {
+    public int SelDepth { get; set; }
+
+    public override string Id
+    {
+      get { return "seldepth"; }
+    }
+
+  }
+}
