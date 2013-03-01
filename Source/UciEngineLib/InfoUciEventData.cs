89c0a53d35b806873e3087b0db2745e321a6756e
Gennadii Saltyshchak <GennadyGS@gmail.com>
2013-03-01 10:45:40 +0200

    Register info event fields

--- Source/UciEngineLib/InfoUciEventData.cs
+++ Source/UciEngineLib/InfoUciEventData.cs
@@ -3,10 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ChessBombDetector.Fields;
 
 namespace ChessBombDetector
 {
   class InfoUciEventData : UciEventData<InfoUciEventFieldId>
   {
+
+    private static void RegisterFields()
+    {
+      RegisterField<IntegerUciEventField>(InfoUciEventFieldId.Depth);
+      RegisterField<IntegerUciEventField>(InfoUciEventFieldId.SelDepth);
+      RegisterField<IntegerUciEventField>(InfoUciEventFieldId.Time);
+      RegisterField<IntegerUciEventField>(InfoUciEventFieldId.Nodes);
+      // RegisterField<IntegerUciEventField>(InfoUciEventFieldId.Pv);
+      RegisterField<IntegerUciEventField>(InfoUciEventFieldId.MultiPv);
+      // RegisterField<IntegerUciEventField>(InfoUciEventFieldId.Score);
+      RegisterField<WordUciEventField>(InfoUciEventFieldId.CurrMove);
+      RegisterField<IntegerUciEventField>(InfoUciEventFieldId.CurrMoveNumber);
+      RegisterField<IntegerUciEventField>(InfoUciEventFieldId.HashFull);
+      RegisterField<IntegerUciEventField>(InfoUciEventFieldId.Nps);
+      RegisterField<IntegerUciEventField>(InfoUciEventFieldId.TbHits);
+      RegisterField<IntegerUciEventField>(InfoUciEventFieldId.CpuLoad);
+      RegisterField<StringUciEventField>(InfoUciEventFieldId.String);
+      // RegisterField<IntegerUciEventField>(InfoUciEventFieldId.Refutation);
+      // RegisterField<IntegerUciEventField>(InfoUciEventFieldId.CurrLine);
+    }
+
+    static InfoUciEventData()
+    {
+      RegisterFields();
+    }
   }
 }
