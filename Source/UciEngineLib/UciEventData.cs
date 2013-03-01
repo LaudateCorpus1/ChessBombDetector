eb93e3a82a511383177305f11bdd746cd8e7a871
Gennadii Saltyshchak <GennadyGS@gmail.com>
2013-03-01 10:43:34 +0200

    Added event fields

--- Source/UciEngineLib/UciEventData.cs
+++ Source/UciEngineLib/UciEventData.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ChessBombDetector.Fields;
 using ChessBombDetector.Utils;
 
 namespace ChessBombDetector
