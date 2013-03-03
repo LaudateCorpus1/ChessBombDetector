4c07db8dab90d0d58c385427ab4199435b32dd5b
Gennadii Saltyshchak <GennadyGS@gmail.com>
2013-03-03 12:38:18 +0200

    UciEventData refactoring

--- Source/UciEngineLib/UciEventData.cs
+++ Source/UciEngineLib/UciEventData.cs
@@ -11,28 +11,21 @@ namespace ChessBombDetector
     class UciEventData<TFieldId> where TFieldId : struct
     {
 
-        private static readonly FactoryRegistry<TFieldId, UciEventField> _fieldFactoryRegistry = new FactoryRegistry<TFieldId, UciEventField>();
-
-        private static readonly Dictionary<TFieldId, Action<UciEventData<TFieldId>, UciEventField>> _fieldSetterRegistry = new Dictionary<TFieldId, Action<UciEventData<TFieldId>, UciEventField>>();
+        private static readonly Dictionary<TFieldId, Func<UciEventData<TFieldId>, UciEventField>> _fieldFactoryRegistry = new Dictionary<TFieldId, Func<UciEventData<TFieldId>, UciEventField>>();
 
         private readonly IDictionary<TFieldId, UciEventField> _fields = new Dictionary<TFieldId, UciEventField>();
 
-        private void TryInvokeFieldSetter(TFieldId fieldId)
-        {
-            
-        }
-
-
         protected static void RegisterField<TFieldClass>(TFieldId fieldId, Action<UciEventData<TFieldId>, TFieldClass> fieldSetter = null) where TFieldClass : UciEventField, new()
         {
-            _fieldFactoryRegistry.Register<TFieldClass>(fieldId);
-            if (fieldSetter != null)
-            {
-                // TODO: Delete explicit type convertion
-                Action<UciEventData<TFieldId>, UciEventField> action =
-                    (obj, field) => fieldSetter(obj, (TFieldClass)field);
-                _fieldSetterRegistry.Add(fieldId, action);
-            }
+            _fieldFactoryRegistry.Add(fieldId, 
+                obj =>
+                {
+                    var field = new TFieldClass();
+                    if (fieldSetter != null)
+                        fieldSetter(obj, field);
+                    return field;
+                }
+            );
         }
 
         protected UciEventField FindField(TFieldId id)
