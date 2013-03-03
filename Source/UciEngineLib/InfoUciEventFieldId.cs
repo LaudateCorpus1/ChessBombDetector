2876d27b666e58ecdf7b70e78159d372c118e46d
Gennadii Saltyshchak <GennadyGS@gmail.com>
2013-03-03 19:31:53 +0200

    Add descriprions for InfoUciEventFieldId enumeration

--- Source/UciEngineLib/InfoUciEventFieldId.cs
+++ Source/UciEngineLib/InfoUciEventFieldId.cs
@@ -1,28 +1,60 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ChessBombDetector
 {
-  enum InfoUciEventFieldId
-  {
-    Depth,
-    SelDepth,
-    Time,
-    Nodes,
-    Pv,
-    MultiPv,
-    Score,
-    CurrMove,
-    CurrMoveNumber,
-    HashFull,
-    Nps,
-    TbHits,
-    CpuLoad,
-    String,
-    Refutation,
-    CurrLine
-  }
+    enum InfoUciEventFieldId
+    {
+        [Description("depth")]
+        Depth,
+
+        [Description("seldepth")]
+        SelDepth,
+
+        [Description("time")]
+        Time,
+
+        [Description("nodes")]
+        Nodes,
+
+        [Description("pv")]
+        Pv,
+
+        [Description("multipv")]
+        MultiPv,
+
+        [Description("score")]
+        Score,
+
+        [Description("currmove")]
+        CurrMove,
+
+        [Description("currmovenumber")]
+        CurrMoveNumber,
+
+        [Description("hashfull")]
+        HashFull,
+
+        [Description("nps")]
+        Nps,
+
+        [Description("tbhits")]
+        TbHits,
+
+        [Description("sbhits")]
+        CpuLoad,
+
+        [Description("string")]
+        String,
+
+        [Description("Refutation")]
+        Refutation,
+
+        [Description("currline")]
+        CurrLine
+    }
 }
