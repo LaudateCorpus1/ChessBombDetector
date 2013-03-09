25bbdf066775e9d1559086960fba779514dc7d6e
Gennadii Saltyshchak <GennadyGS@gmail.com>
2013-03-09 15:46:31 +0200

    Add classes for "id" event

---
+++ Source/UciEngineLib/Events/IdUciEventData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChessBombDetector.EventFields;
+
+namespace ChessBombDetector.Events
+{
+    internal class IdUciEventData : UciEventData<IdUciEventFieldId>
+    {
+        private static void RegisterFields()
+        {
+            RegisterField<StringUciEventField>(IdUciEventFieldId.Name,
+                                               (data, field) => ((IdUciEventData) data).Name = field);
+            RegisterField<StringUciEventField>(IdUciEventFieldId.Author,
+                                               (data, field) => ((IdUciEventData) data).Author = field);
+        }
+
+        static IdUciEventData()
+        {
+            RegisterFields();
+        }
+
+        public StringUciEventField Name { get; private set; }
+        public StringUciEventField Author { get; private set; }
+    }
+}
