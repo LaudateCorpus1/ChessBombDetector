8218c3cacb1585705bc2b8952a42ef1efbc4fb4a
Gennadii Saltyshchak <GennadyGS@gmail.com>
2013-03-11 14:59:04 +0200

    Add unit test project UciEngineLibTest

---
+++ Source/UciEngineLibTest/EventParserTest.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+using ChessBombDetector;
+using ChessBombDetector.Events;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UciEngineLibTest
+{
+  [TestClass]
+  public class EventParserTest
+  {
+    
+    private static TEventType ParseEvent<TEventType>(EventType eventType, string line) where TEventType: Event, new()
+    {
+      EngineEventParser eventParser = new EngineEventParser();
+      eventParser.RegisterEventType<TEventType>(eventType);
+      Event ev = eventParser.ParseEvent(line);
+      Assert.AreEqual(ev.Type, eventType);
+      Assert.IsInstanceOfType(ev, typeof(TEventType));
+      return (TEventType) ev;
+
+    }
+    
+    [TestMethod]
+    public void TestParseEventInfoString()
+    {
+      InfoEvent ev = ParseEvent<InfoEvent>(EventType.Info, "info string 128 MB Hash");
+      Assert.AreEqual("128 MB Hash", ev.String.Value);
+      Assert.AreEqual(ev.Fields().Count(), 1);
+    }
+
+    [TestMethod]
+    public void TestParseEventUciOk()
+    {
+      ParseEvent<UciOkEvent>(EventType.UciOk, "uciok");
+    }
+
+    [TestMethod]
+    public void TestParseEventReadyOk()
+    {
+      ParseEvent<ReadyOkEvent>(EventType.ReadyOk, "readyok");
+    }
+
+    [TestMethod]
+    public void TestParseEventReadyIdName()
+    {
+      IdEvent ev = ParseEvent<IdEvent>(EventType.Id, "id name Houdini 3a Pro w32");
+      Assert.AreEqual("Houdini 3a Pro w32", ev.Name.Value);
+      Assert.AreEqual(ev.Fields().Count(), 1);
+    }
+
+    [TestMethod]
+    public void TestParseEventReadyIdAuthor()
+    {
+      IdEvent ev = ParseEvent<IdEvent>(EventType.Id, "id author Robert Houdart");
+      Assert.AreEqual("Robert Houdart", ev.Author.Value);
+      Assert.AreEqual(ev.Fields().Count(), 1);
+    }
+  }
+}
---
+++ Source/UciEngineLibTest/StringReaderExtensionTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ChessBombDetector.Utils;
+
+namespace UciEngineLibTest
+{
+  [TestClass]
+  public class StringReaderExtensionTest
+  {
+    [TestMethod]
+    public void TestReadWord()
+    {
+      StringReader reader = new StringReader("string 128 MB Hash");
+      Assert.AreEqual("string", reader.ReadWord());
+      Assert.AreEqual("128", reader.ReadWord());
+      Assert.AreEqual("MB", reader.ReadWord());
+      Assert.AreEqual("Hash", reader.ReadWord());
+      Assert.AreEqual(null, reader.ReadWord());
+      Assert.AreEqual(null, reader.ReadWord());
+    }
+
+    [TestMethod]
+    public void TestReadWordNull()
+    {
+      StringReader reader = new StringReader("");
+      Assert.AreEqual(null, reader.ReadWord());
+      Assert.AreEqual(null, reader.ReadWord());
+    }
+
+    [TestMethod]
+    public void TestReadWordSpace()
+    {
+      StringReader reader = new StringReader("  ");
+      Assert.AreEqual("", reader.ReadWord());
+      Assert.AreEqual("", reader.ReadWord());
+      Assert.AreEqual(null, reader.ReadWord());
+      Assert.AreEqual(null, reader.ReadWord());
+    }
+
+    [TestMethod]
+    public void TestReadWordInternalSpace()
+    {
+      StringReader reader = new StringReader(" 1 2 3  ");
+      Assert.AreEqual("", reader.ReadWord());
+      Assert.AreEqual("1", reader.ReadWord());
+      Assert.AreEqual("2", reader.ReadWord());
+      Assert.AreEqual("3", reader.ReadWord());
+      Assert.AreEqual("", reader.ReadWord());
+      Assert.AreEqual(null, reader.ReadWord());
+      Assert.AreEqual(null, reader.ReadWord());
+    }
+
+  }
+}
