7a6ac97d45006f421f85a7f39018102570042683
Gennadii Saltyshchak <GennadyGS@gmail.com>
2013-03-12 10:07:10 +0200

    BestMove event implementation

--- Source/UciEngineLib/Events/BestMoveEvent.cs
+++ Source/UciEngineLib/Events/BestMoveEvent.cs
@@ -1,15 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ChessBombDetector.Utils;
 
 namespace ChessBombDetector.Events
 {
-    public class BestMoveEvent: Event
+  public class BestMoveEvent : Event
+  {
+    public string Move { get; private set; }
+    public string Ponder { get; private set; }
+
+    public BestMoveEvent()
+      : base(EventType.BestMove)
     {
-        public BestMoveEvent() : base(EventType.BestMove)
-        {
-        }
     }
+
+    public override void ReadFromStream(StringReader reader)
+    {
+      Move = reader.ReadWord();
+      string word = reader.ReadWord();
+      if (word == null)
+        return;
+      if (word != "ponder")
+        throw new InvalidDataException(String.Format("Inexpected word in BestMove variation: {0}", word));
+      Ponder = reader.ReadWord();
+    }
+  }
 }
