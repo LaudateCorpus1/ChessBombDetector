26a7a3b02fdbffb4f0bb80bb69c35feeefb88e9c
Gennadii Saltyshchak <GennadyGS@gmail.com>
2013-03-13 18:00:48 +0200

    Added test TestParseEventInfo

--- Source/UciEngineLibTest/EventParserTest.cs
+++ Source/UciEngineLibTest/EventParserTest.cs
@@ -63,7 +63,7 @@ namespace UciEngineLibTest
     {
       OptionEvent ev = ParseEvent<OptionEvent>(EventType.Option, "option name Style type combo default Normal var Solid var Normal var Risky");
       Assert.AreEqual("Style", ev.NameField.Value);
-      Assert.IsTrue(OptionType.Combo == ev.TypeField.Value);
+      Assert.IsTrue(OptionTypeEventField.OptionType.Combo == ev.TypeField.Value);
       Assert.AreEqual("Normal", ev.DefaultField.Value);
       Assert.IsNotNull(ev.VarField);
       WordEventField[] fields = ev.VarField.Fields.ToArray();
@@ -72,6 +72,30 @@ namespace UciEngineLibTest
       Assert.AreEqual("Normal", fields[1].Value);
       Assert.AreEqual("Risky", fields[2].Value);
       Assert.AreEqual(ev.Fields().Count(), 4);
+    }
+
+    [TestMethod]
+    public void TestParseEventInfo()
+    {
+      InfoEvent ev = ParseEvent<InfoEvent>(EventType.Info, "info multipv 1 depth 4 seldepth 16 score cp -13 time 7 nodes 13957 nps 1993000 tbhits 0 hashfull 0 pv f8d8 b3c2 h3f5 c2e4 b5b4 c3b4 d6b4");
+      Assert.AreEqual(1, ev.MultiPv.Value);
+      Assert.AreEqual(4, ev.Depth.Value);
+      Assert.AreEqual(16, ev.SelDepth.Value);
+      Assert.IsTrue(ScoreEventField.ScoreType.Cp == ev.Score.Type);
+      Assert.AreEqual(-13, ev.Score.CpScore);
+      Assert.AreEqual(7, ev.Time.Value);
+      Assert.AreEqual(13957, ev.Nodes.Value);
+      Assert.AreEqual(1993000, ev.Nps.Value);
+      Assert.AreEqual(0, ev.TbHits.Value);
+      Assert.AreEqual(0, ev.HashFull.Value);
+      Assert.AreEqual(7, ev.Pv.Moves.Length);
+      Assert.AreEqual("f8d8", ev.Pv.Moves[0]);
+      Assert.AreEqual("b3c2", ev.Pv.Moves[1]);
+      Assert.AreEqual("h3f5", ev.Pv.Moves[2]);
+      Assert.AreEqual("c2e4", ev.Pv.Moves[3]);
+      Assert.AreEqual("b5b4", ev.Pv.Moves[4]);
+      Assert.AreEqual("c3b4", ev.Pv.Moves[5]);
+      Assert.AreEqual("d6b4", ev.Pv.Moves[6]);
     }
   }
 }
