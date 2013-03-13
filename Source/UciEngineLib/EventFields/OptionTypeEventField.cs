3bec0824f20a056b92cfb8110c238d071556d0f1
Gennadii Saltyshchak <GennadyGS@gmail.com>
2013-03-13 17:54:25 +0200

    Move OptionType into OptionTypeEventField

--- Source/UciEngineLib/EventFields/OptionTypeEventField.cs
+++ Source/UciEngineLib/EventFields/OptionTypeEventField.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,24 @@ namespace ChessBombDetector.EventFields
 {
   public class OptionTypeEventField : EventField
   {
+    public enum OptionType
+    {
+      [Description("check")]
+      Check,
+
+      [Description("spin")]
+      Spin,
+
+      [Description("combo")]
+      Combo,
+
+      [Description("button")]
+      Button,
+
+      [Description("string")]
+      String
+    }
+
     public OptionType Value { get; private set; }
 
     public override void ReadFromStream(StringReader reader)
