3b0223b92b60f2bc68e1144ee6d85596bbc0a2d9
Gennadii Saltyshchak <GennadyGS@gmail.com>
2013-03-13 17:53:55 +0200

    ScoreEventField implementation

--- Source/UciEngineLib/EventFields/ScoreEventField.cs
+++ Source/UciEngineLib/EventFields/ScoreEventField.cs
@@ -1,13 +1,45 @@
 ﻿using System;
+using System.ComponentModel;
 using System.IO;
+using ChessBombDetector.Utils;
 
 namespace ChessBombDetector.EventFields
 {
-    public class ScoreEventField : EventField
+  public class ScoreEventField : EventField
+  {
+    public enum ScoreType
     {
-        public override void ReadFromStream(StringReader reader)
-        {
-            throw new NotImplementedException();
-        }
+      [Description("cp")]
+      Cp,
+
+      [Description("mate")]
+      Mate,
+
+      [Description("lowerbound")]
+      LowerBound,
+
+      [Description("upperbound")]
+      UpperBound
     }
+
+    public ScoreType Type { get; private set; }
+
+    public int CpScore { get; private set; }
+
+    public int MateDepth { get; private set; }
+
+    public override void ReadFromStream(StringReader reader)
+    {
+      Type = EnumDescriptionToValueMapper<ScoreType>.GetValueByDescription(reader.ReadWord());
+      switch (Type)
+      {
+        case ScoreType.Cp:
+          CpScore = int.Parse(reader.ReadWord());
+          break;
+        case ScoreType.Mate:
+          MateDepth = int.Parse(reader.ReadWord());
+          break;
+      }
+    }
+  }
 }
