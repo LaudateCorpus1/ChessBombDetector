b2cea881cdcc76590d38b47ef44e2b6389768831
Gennadii Saltyshchak <GennadyGS@gmail.com>
2013-04-14 15:41:52 +0300

    Add UciEngineController application

---
+++ Source/UciEngineController/MainForm.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace UciEngineController
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+      this.GoButton = new System.Windows.Forms.Button();
+      this.StopButton = new System.Windows.Forms.Button();
+      this.SuspendLayout();
+      // 
+      // GoButton
+      // 
+      this.GoButton.Location = new System.Drawing.Point(12, 22);
+      this.GoButton.Name = "GoButton";
+      this.GoButton.Size = new System.Drawing.Size(75, 23);
+      this.GoButton.TabIndex = 0;
+      this.GoButton.Text = "Go";
+      this.GoButton.UseVisualStyleBackColor = true;
+      this.GoButton.Click += new System.EventHandler(this.GoButton_Click);
+      // 
+      // StopButton
+      // 
+      this.StopButton.Location = new System.Drawing.Point(104, 22);
+      this.StopButton.Name = "StopButton";
+      this.StopButton.Size = new System.Drawing.Size(75, 23);
+      this.StopButton.TabIndex = 1;
+      this.StopButton.Text = "Stop";
+      this.StopButton.UseVisualStyleBackColor = true;
+      this.StopButton.Click += new System.EventHandler(this.StopButton_Click);
+      // 
+      // MainForm
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(402, 201);
+      this.Controls.Add(this.StopButton);
+      this.Controls.Add(this.GoButton);
+      this.Name = "MainForm";
+      this.Text = "UCI Engine Controller";
+      this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+      this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button GoButton;
+        private System.Windows.Forms.Button StopButton;
+    }
+}
+
---
+++ Source/UciEngineController/MainForm.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using UciEngineLib;
+using log4net;
+
+namespace UciEngineController
+{
+  public partial class MainForm : Form
+  {
+    private static readonly ILog Log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+    private readonly Process _engineProcess;
+
+    private readonly Engine _engine;
+
+    public MainForm()
+    {
+      InitializeComponent();
+      _engineProcess = new Process
+        {
+          StartInfo =
+            {
+              FileName = ConfigurationManager.AppSettings["EngineFileName"],
+              UseShellExecute = false,
+              RedirectStandardOutput = true,
+              RedirectStandardInput = true,
+              CreateNoWindow = true
+            }
+        };
+      _engineProcess.Start();
+      _engine = new Engine(_engineProcess.StandardOutput, _engineProcess.StandardInput);
+      _engine.ExceptionEvent += (sender, args) =>
+        Log.Error(String.Format("Error handling output line'{0}'", args.Data.Item1), args.Data.Item2);
+      _engine.InfoEvent += (sender, args) =>
+          {
+            if (args.Data.CurrMove != null)
+            {
+              Log.InfoFormat("CurrMove={0}", args.Data.CurrMove.Value);
+            }
+          };
+      while (_engineProcess.StandardOutput.ReadLine() != "")
+      {
+      }
+      _engine.Run();
+    }
+
+    private void GoButton_Click(object sender, EventArgs e)
+    {
+      Go();
+    }
+
+    private void StopButton_Click(object sender, EventArgs e)
+    {
+      Stop();
+    }
+
+    private void Go()
+    {
+      _engine.Go();
+    }
+
+    private void Stop()
+    {
+      _engine.Stop();
+    }
+
+    private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+    {
+      try
+      {
+        _engine.Quit();
+        _engine.WaitForEventProcessor();
+        _engineProcess.WaitForExit();
+      }
+      finally
+      {
+        _engineProcess.Close();
+      }
+    }
+  }
+}
