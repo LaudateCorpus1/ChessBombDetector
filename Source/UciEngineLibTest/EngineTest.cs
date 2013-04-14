23ad9553d9ecd795f1a8d847bf9cf291a9f0940f
Gennadii Saltyshchak <GennadyGS@gmail.com>
2013-04-14 15:41:32 +0300

    EnfineTest: Verify error absence

--- Source/UciEngineLibTest/EngineTest.cs
+++ Source/UciEngineLibTest/EngineTest.cs
@@ -10,24 +10,27 @@ using UciEngineLib;
 namespace UciEngineLibTest
 {
 
-  [TestClass]
-  public class EngineTest
-  {
-    private Stream CreateStreamFromString(string str)
+    [TestClass]
+    public class EngineTest
     {
-      return new MemoryStream(Encoding.Default.GetBytes(str ?? ""));
-    }
+        private Stream CreateStreamFromString(string str)
+        {
+            return new MemoryStream(Encoding.Default.GetBytes(str ?? ""));
+        }
 
-    [TestMethod]
-    public void TestEngineEventHandler()
-    {
-      Stream stream = CreateStreamFromString(Resource.EngineEvents);
-      StreamReader reader = new StreamReader(stream); 
-      using (Engine engine = new Engine(reader, null))
-      {
-        engine.Run();
-        engine.WaitForEventProcessor();
-      }
+        [TestMethod]
+        public void TestEngineEventHandler()
+        {
+            int errorCount = 0;
+            Stream stream = CreateStreamFromString(Resource.EngineEvents);
+            StreamReader reader = new StreamReader(stream);
+            using (Engine engine = new Engine(reader, null))
+            {
+                engine.ExceptionEvent += (sender, args) => errorCount++;
+                engine.Run();
+                engine.WaitForEventProcessor();
+            }
+            Assert.AreEqual(0, errorCount);
+        }
     }
-  }
 }
