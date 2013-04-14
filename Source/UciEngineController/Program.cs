a08ee23d0789257b13b2e5681b073ccdf56c9425
Gennadii Saltyshchak <GennadyGS@gmail.com>
2013-04-14 17:24:41 +0300

    Configure logging

--- Source/UciEngineController/Program.cs
+++ Source/UciEngineController/Program.cs
@@ -6,17 +6,18 @@ using System.Windows.Forms;
 
 namespace UciEngineController
 {
-    static class Program
+  static class Program
+  {
+    /// <summary>
+    /// The main entry point for the application.
+    /// </summary>
+    [STAThread]
+    static void Main()
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
-        }
+      log4net.Config.XmlConfigurator.Configure();
+      Application.EnableVisualStyles();
+      Application.SetCompatibleTextRenderingDefault(false);
+      Application.Run(new MainForm());
     }
+  }
 }
