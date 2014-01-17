e5ec02d7b1f5d8489a2a0e10a5c49aa9ee268eb9
Gennadii Saltyshchak <GennadyGS@gmail.com>
2014-01-17 14:02:01 +0200

    Ckeanup code

--- Source/UciEngineLibTest/EngineTest.cs
+++ Source/UciEngineLibTest/EngineTest.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
+﻿using System.IO;
 using System.Text;
-using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using UciEngineLib;
 
 namespace UciEngineLibTest
 {
-
     [TestClass]
     public class EngineTest
     {
