6e834dbd628c6ecc04177a2d4208fc6c86662540
Gennadii Saltyshchak <GennadyGS@gmail.com>
2014-01-20 17:20:57 +0200

    Set tab size to 4

--- Source/UciEngineLibTest/EngineTest.cs
+++ Source/UciEngineLibTest/EngineTest.cs
@@ -5,27 +5,27 @@ using UciEngineLib;
 
 namespace UciEngineLibTest
 {
-    [TestClass]
-    public class EngineTest
+  [TestClass]
+  public class EngineTest
+  {
+    private Stream CreateStreamFromString(string str)
     {
-        private Stream CreateStreamFromString(string str)
-        {
-            return new MemoryStream(Encoding.Default.GetBytes(str ?? ""));
-        }
-
-        [TestMethod]
-        public void TestEngineEventHandler()
-        {
-            int errorCount = 0;
-            Stream stream = CreateStreamFromString(Resource.EngineEvents);
-            var reader = new StreamReader(stream);
-            using (var engine = new Engine(reader, null))
-            {
-                engine.ExceptionEvent += (sender, args) => errorCount++;
-                engine.Run();
-                engine.WaitForEventProcessor();
-            }
-            Assert.AreEqual(0, errorCount);
-        }
+      return new MemoryStream(Encoding.Default.GetBytes(str ?? ""));
     }
+
+    [TestMethod]
+    public void TestEngineEventHandler()
+    {
+      int errorCount = 0;
+      Stream stream = CreateStreamFromString(Resource.EngineEvents);
+      var reader = new StreamReader(stream);
+      using (var engine = new Engine(reader, null))
+      {
+        engine.ExceptionEvent += (sender, args) => errorCount++;
+        engine.Run();
+        engine.WaitForEventProcessor();
+      }
+      Assert.AreEqual(0, errorCount);
+    }
+  }
 }
